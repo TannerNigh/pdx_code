6621e93496b6bccf2272e0689eb54529b4155422
rustano <dialist-eustacy@protonmail.com>
2022-08-25 13:44:40 -0700

    Created unit_1 python file and began working with strings

---
+++ programming_101/unit_1/unit_1.py
@@ -0,0 +1,30 @@
+# This is a comment. 
+# Comments are useful for organizing, explaining, 
+# and excluding certain lines of code
+
+
+# Using a print statement to say welcome to python
+print("Welcome to Python")
+
+# Saying hello using a print statement
+print('Hello')
+
+# Print() is a built in function and allows for both single and double quotes
+print('This is an example of me using single quotes')
+print("This is an example of me using double quotes")
+
+# You can also use triple quoted string to span multiple files like so:
+print("""
+I am making use of triple quotes
+so that I can span multiple lines
+""")
+
+# String concatenation comibes strings using the + operator
+print("I" + "am" "using" + "concatenation")
+
+# Methods are functions that manipulate the peice of data for example:
+# capitalize() method example
+print("i am capatilizing the first word using capitalize()".capitalize())
+
+# lower() method example
+print("I am using the lower() method to turn the first letter lowercase".lower())
